0e6c3b6a459f3397a105baeca6ad7f19d20cbe26
acin91 <z.aliakseyeu@gmail.com>
2014-02-06 20:23:48 +0300

    api requestor refactoring

--- lib/YandexMoney/ApiRequestor.php
+++ lib/YandexMoney/ApiRequestor.php
@@ -1,31 +1,64 @@
 <?php
 
-class YM_ApiRequestor {
+namespace Yandex\YandexMoney;
 
-    const YM_USER_AGENT = 'yamolib-php';
+/**
+ * 
+ */
+class ApiRequestor 
+{
+    /**
+     * 
+     */
+    const USER_AGENT = 'yamolib-php';
 
+    /**
+     * @var string
+     */
     private $accessToken;
+
+    /**
+     * @var string
+     */
     private $logFile;
 
-    public function __construct($accessToken = null, $logFile = null) {
+    /**
+     * @param string $accessToken
+     * @param string $logFile
+     */
+    public function __construct($accessToken = null, $logFile = null)
+    {
         $this->accessToken = $accessToken;
         $this->logFile = $logFile;
     }
 
-    public function request($uri, $params = null, $expectResponseBody = true) {
+    /**
+     * @param string $uri
+     * @param string $params
+     * @return array
+     */
+    public function request($uri, $params = null, $expectResponseBody = true)
+    {
         list($rbody, $rcode) = $this->_curlRequest($uri, $params);
         return $this->_interpretResponse($rbody, $rcode, $expectResponseBody);
     }
 
-    private function _curlRequest($uri, $params) {
+    /**
+     * @param string $uri
+     * @param string $params
+     * @return array
+     */
+    private function _curlRequest($uri, $params)
+    {
         $curl = curl_init($uri);
 
         $headers[] = 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8;';
-        if ($this->accessToken)
+        if ($this->accessToken) {
             $headers[] = 'Authorization: Bearer ' . $this->accessToken;
+        }
         curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
 
-        curl_setopt($curl, CURLOPT_USERAGENT, self::YM_USER_AGENT);
+        curl_setopt($curl, CURLOPT_USERAGENT, self::USER_AGENT);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         //        curl_setopt($curl, CURLOPT_FORBID_REUSE, true);
         curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 30);
@@ -34,7 +67,7 @@ class YM_ApiRequestor {
         curl_setopt($curl, CURLOPT_POSTFIELDS, $params);
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, true);
         //        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, true);
-        curl_setopt($curl, CURLOPT_CAINFO, dirname(__FILE__) . '/../data/ca-certificate.crt');
+        curl_setopt($curl, CURLOPT_CAINFO, dirname(__FILE__) . '/../Resources/data/ca-certificate.crt');
                                     
         $this->_log($this->_makeRequestLogMessage($uri, $params));  
 
@@ -54,7 +87,15 @@ class YM_ApiRequestor {
         return array($rbody, $rcode);
     }
 
-    private function _interpretResponse($rbody, $rcode, $expectResponseBody) {
+    /**
+     * @param string $rbody
+     * @param int $rcode
+     * @return array $response
+     * 
+     * @throws \Yandex\YandexMoney\Exception\ApiException
+     */
+    private function _interpretResponse($rbody, $rcode, $expectResponseBody)
+    {
         if ($rcode < 200 || $rcode >= 300) {
             $this->_handleApiError($rbody, $rcode, $expectResponseBody);
         }
@@ -62,7 +103,7 @@ class YM_ApiRequestor {
         try {
             $resp = json_decode($rbody, true);            
         } catch (Exception $e) {
-            throw new YM_ApiError("Invalid response body from API: $rbody (HTTP response code was $rcode)", $rcode, $rbody);
+            throw new Exception\ApiException("Invalid response body from API: $rbody (HTTP response code was $rcode)", $rcode, $rbody);
         }    
 
         // Only for PHP 5 >= 5.3.0
@@ -71,13 +112,20 @@ class YM_ApiRequestor {
         // }
 
         if ($resp === null && $expectResponseBody) {
-            throw new YM_ApiError("Server response body is null: $rbody (HTTP response code was $rcode)", $rcode, $rbody);
+            throw new Exception\ApiException("Server response body is null: $rbody (HTTP response code was $rcode)", $rcode, $rbody);
         }
         
         return $resp;
     }
 
-    private function _handleCurlError($errno, $message) {
+    /**
+     * @param int $errno
+     * @param string $message
+     * 
+     * @throws \Yandex\YandexMoney\Exception\ApiConnectionException
+     */
+    private function _handleCurlError($errno, $message)
+    {
         switch ($errno) {
             case CURLE_COULDNT_CONNECT:
             case CURLE_COULDNT_RESOLVE_HOST:
@@ -93,75 +141,120 @@ class YM_ApiRequestor {
         }
 
         $msg .= "\n\n(Network error: $message)";
-        throw new YM_ApiConnectionError($msg);
+
+        throw new Exception\ApiConnectionException($msg);
     }
 
-    private function _handleApiError($rbody, $rcode, $resp) {
+    /**
+     * @param string $rbody
+     * @param string $rcode
+     * @param string $resp
+     * 
+     * @throws \Yandex\YandexMoney\Exception\ApiException
+     * @throws \Yandex\YandexMoney\Exception\InvalidTokenException
+     * @throws \Yandex\YandexMoney\Exception\InsufficientScopeException
+     * @throws \Yandex\YandexMoney\Exception\InternalServerErrorException
+     */
+    private function _handleApiError($rbody, $rcode, $resp)
+    {
         switch ($rcode) {
             case 400:
-                throw new YM_ApiError("Invalid request error", $rcode, $rbody, $resp);
+                throw new Exception\ApiException('Invalid request error', $rcode, $rbody, $resp);
             case 401:
-                throw new YM_InvalidTokenError("Nonexistent, expired, or revoked token specified.", $rcode, $rbody, $resp);
+                throw new Exception\InvalidTokenException('Nonexistent, expired, or revoked token specified.', $rcode, $rbody, $resp);
             case 403:
-                throw new YM_InsufficientScopeError("The token does not have permissions for the requested operation.",
+                throw new Exception\InsufficientScopeException('The token does not have permissions for the requested operation.',
                     $rcode, $rbody, $resp);
             case 500:
-                throw new YM_InternalServerError("It is a technical error occurs, the server responds with the HTTP code
-                    500 Internal Server Error. The application should repeat the request with the same parameters later.",
+                throw new Exception\InternalServerErrorException('It is a technical error occurs, the server responds with the HTTP code
+                    500 Internal Server Error. The application should repeat the request with the same parameters later.',
                     $rcode, $rbody, $resp);
             default:
-                throw new YM_ApiError("Unknown API response error. You should inform your software developer.",
+                throw new Exception\ApiException('Unknown API response error. You should inform your software developer.',
                     $rcode, $rbody, $resp);
         }
     }
 
-    private function _log($message) {
+    /**
+     * @param string $message
+     * 
+     * @throws \Yandex\YandexMoney\Exception\Exception
+     */
+    private function _log($message)
+    {
         $f = $this->logFile;
         if ($f !== null) {
             if (file_exists($f)) {
-                if (!is_file($f))
-                    throw new YM_Error("log file $f is not a file");
-                if (!is_writable($f))
-                    throw new YM_Error("log file $f is not writable");
+                if (!is_file($f)) {
+                    throw new Exception\Exception("log file $f is not a file");
+                }
+                if (!is_writable($f)) {
+                    throw new Exception\Exception("log file $f is not writable");
+                }
             }
                 
-            if (!$handle = fopen($f, 'a'))
-                throw new YM_Error("couldn't open log file $f for appending");
+            if (!$handle = fopen($f, 'a')) {
+                throw new Exception\Exception("couldn't open log file $f for appending");
+            }
             
             $time = '[' . date("Y-m-d H:i:s") . '] ';
-            if (fwrite($handle, $time . $message . "\r\n") === FALSE)
-                throw new YM_Error("couldn't fwrite message log to $f"); 
+            if (fwrite($handle, $time . $message . "\r\n") === false) {
+                throw new Exception\Exception("couldn't fwrite message log to $f"); 
+            }
             
             fclose($handle);                                
         }
     }
 
-    private function _makeRequestLogMessage($uri, $params) {
-        if ($this->logFile === null)  
+    /**
+     * @param string $uri
+     * @param string $params
+     * 
+     * @return string
+     */
+    private function _makeRequestLogMessage($uri, $params)
+    {
+        if ($this->logFile === null) {
             return '';
+        }
 
         $m = "request: " . $uri . '; ';        
 
         $token = $this->accessToken;
-        if (isset($token))
+        if (isset($token)) {
             $m = $m . 'token = *' . substr($token, -4) . '; ';
+        }
 
         parse_str($params);        
-        if (isset($to))
+        if (isset($to)) {
             $m = $m . 'param to = *' . substr($to, -4) . '; ';
+        }
         
-        if (isset($pattern_id))
+        if (isset($pattern_id)) {
             $m = $m . 'param pattern_id = ' . $pattern_id . '; ';
+        }
         
-        if (isset($request_id))
+        if (isset($request_id)) {
             $m = $m . 'param request_id = *' . substr($request_id, -4) . '; ';
+        }
 
         return $m;
     }
 
-    private function _makeResponseLogMessage($uri, $params, $code, $errno, $error) { 
-        if ($this->logFile === null)  
+    /**
+     * @param string $uri
+     * @param string $params
+     * @param int $code
+     * @param int $errno
+     * @param string $error
+     * 
+     * @return string
+     */
+    private function _makeResponseLogMessage($uri, $params, $code, $errno, $error)
+    { 
+        if ($this->logFile === null) {
             return '';
+        }
 
         $m = $this->_makeRequestLogMessage($uri, $params);
         $m = str_replace('request: ', 'response: ', $m);
