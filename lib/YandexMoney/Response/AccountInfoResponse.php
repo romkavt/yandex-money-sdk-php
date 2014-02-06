86dd9246f0a35259960bf78b804f4fa245d7623d
acin91 <z.aliakseyeu@gmail.com>
2014-02-06 20:21:47 +0300

    response interface and response refactoring origins

--- lib/YandexMoney/Response/AccountInfoResponse.php
+++ lib/YandexMoney/Response/AccountInfoResponse.php
@@ -1,62 +1,112 @@
 <?php
 
-class YM_AccountInfoResponse {
+namespace Yandex\YandexMoney\Response;
 
+/**
+ * 
+ */
+class AccountInfoResponse implements ResponseInterface
+{
+    /**
+     * @var string
+     */
     protected $account;
+    
+    /**
+     * @var string
+     */
     protected $balance;
+
+    /**
+     * @var string
+     */
     protected $currency;
+
+    /**
+     * @var boolean
+     */
     protected $identified;
+
+    /**
+     * @var boolean
+     */
     protected $accountType;
 
-    public function __construct($accountInfoArray) {
-        if (isset($accountInfoArray['account']))
-            $this->account = $accountInfoArray['account'];
-        if (isset($accountInfoArray['balance']))
-            $this->balance = $accountInfoArray['balance'];
-        if (isset($accountInfoArray['currency']))
-            $this->currency = $accountInfoArray['currency'];
-        if (isset($accountInfoArray['identified']))
-            $this->identified = (bool)$accountInfoArray['identified'];
-        if (isset($accountInfoArray['account_type']))
-            $this->accountType = $accountInfoArray['account_type'];
+    /**
+     * @param array $accountInfo
+     */
+    public function __construct(array $accountInfo)
+    {
+        if (isset($accountInfo['account'])) {
+            $this->account = $accountInfo['account'];
+        }
+        if (isset($accountInfo['balance'])) {
+            $this->balance = $accountInfo['balance'];
+        }
+        if (isset($accountInfo['currency'])) {
+            $this->currency = $accountInfo['currency'];
+        }
+        if (isset($accountInfo['identified'])) {
+            $this->identified = (bool) $accountInfo['identified'];
+        }
+        if (isset($accountInfo['account_type'])) {
+            $this->accountType = $accountInfo['account_type'];
+        }
     }
 
     /**
      * @return string возвращает номер счета пользователя
      */
-    public function getAccount() {
+    public function getAccount()
+    {
         return $this->account;
     }
 
     /**
      * @return string возвращает количество денег на счету
      */
-    public function getBalance() {
+    public function getBalance()
+    {
         return $this->balance;
     }
 
     /**
      * @return string возвращает код валюты пользователя
      */
-    public function getCurrency() {
+    public function getCurrency()
+    {
         return $this->currency;
     }
 
     /**
      * @return boolean возвращает, идентифицирован ли пользователь в системе
      */
-    public function getIdentified() {
+    public function getIdentified()
+    {
         return $this->identified;
     }
 
     /**
      * @return boolean возвращает тип счета пользователя
      */
-    public function getAccountType() {
+    public function getAccountType()
+    {
         return $this->accountType;
     }
 
-    public function isSuccess() {
+    /**
+     * {@inheritDoc}
+     */
+    public function getError()
+    {
+        return '';
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function isSuccess()
+    {
         return true;
     }
 }
---
+++ lib/YandexMoney/Response/ResponseInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Yandex\YandexMoney\Response;
+
+/**
+ * @author Zmicier Aliakseyeu <z.aliakseyeu@gmail.com>
+ */
+interface ResponseInterface
+{
+    /**
+     * @return string
+     */
+    public function getError();
+
+    /**
+     * @return boolean
+     */
+    public function isSuccess();
+}
