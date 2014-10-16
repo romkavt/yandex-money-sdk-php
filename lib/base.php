862adb8c25cd6e87a79d7e51178d2912e591de9c
Your Name <Rayman.k26@gmail.com>
2014-10-16 12:31:36 +0400

    moving to curl

--- lib/base.php
+++ lib/base.php
@@ -9,16 +9,32 @@ class BaseAPI {
     
     public static function sendRequest($url, $options=array(), $access_token=NULL) {
         $full_url= self::MONEY_URL . $url;
-        if($access_token != NULL) {
-            $headers = array(
-                "Authorization" => sprintf("Bearer %s", $access_token),
-                "User-Agent" => "Yandex.Money.SDK/PHP"
-            );
-        } 
-        else {
-            $headers = array();
+
+        $curl = curl_init($full_url);
+        if($access_token !== NULL) {
+            curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+                "Authorization: Bearer " . $access_token
+            ));
         }
-        $result = \Requests::post($full_url, $headers, $options);
+        curl_setopt ($curl, CURLOPT_USERAGENT, 'Yandex.Money.SDK/PHP');
+        curl_setopt ($curl, CURLOPT_POST, 1);
+        $query = http_build_query($options);
+        curl_setopt ($curl, CURLOPT_POSTFIELDS, $query);
+        curl_setopt ($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($curl, CURLOPT_HEADER, 0);
+        //curl_setopt($curl, CURLOPT_VERBOSE, 1);
+        curl_setopt ($curl, CURLOPT_SSL_VERIFYPEER, true);
+        curl_setopt ($curl, CURLOPT_SSL_VERIFYHOST, 2);
+        curl_setopt($curl, CURLOPT_CAINFO, __DIR__ . "/cacert.pem");
+        $body = curl_exec ($curl);
+
+        $result = new \StdClass();
+        $result->status_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        $result->body = $body;
+
+        curl_close ($curl);
+
         return self::processResult($result);
     }
     protected static function processResult($result) {
